35eb6900c9a28b6783e06ebb5ee0d32c5423ed8f
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2025-09-11 12:32:48 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,15 @@
+function calculo(soma){
+    var reais = 73;
+    var centavos = 39;
+    var soma = reais + centavos;
+    console.log(soma);
+}
+calculo();
+
+function calculo2(subtração){
+    var reais2 = 73;
+    var centavos2 = 39;
+    var subtração = reais2 - centavos2;
+    console.log(subtração);
+}
+calculo2();
