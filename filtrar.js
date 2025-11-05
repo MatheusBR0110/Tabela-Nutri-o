1aa17c7c85649cef454c2adf8793008dbd3533b6
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2025-11-05 09:45:17 -0300

    Add files via upload

---
+++ filtrar.js
@@ -0,0 +1,5 @@
+const campoFiltro = document.querySelector("#filtrar-paciente");
+
+campoFiltro.addEventListener('input', function(){
+    console.log("Tem alguem digitando...");
+});
--- remove-paciente.js
+++ remove-paciente.js
@@ -1,5 +1,16 @@
-pacientes.forEach(function(paciente){
+const tabela = document.querySelector("table");
+
+tabela.addEventListener('dblclick', function(event){
+
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+});
+
+/*pacientes.forEach(function(paciente){
     paciente.addEventListener('dblclick', function(){
     this.remove();
     });
-});
+});*/
