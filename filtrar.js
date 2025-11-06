1f74558b05585c762c90c38145c6a3d4b17b71ea
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2025-11-06 12:32:28 -0300

    Add files via upload

--- filtrar.js
+++ filtrar.js
@@ -1,5 +1,27 @@
 const campoFiltro = document.querySelector("#filtrar-paciente");
 
 campoFiltro.addEventListener('input', function(){
-    console.log("Tem alguem digitando...");
+    this.value;
+
+    const pacientes = document.querySelectorAll(".paciente");
+    
+        //Verificar se tem algo digitado
+        if(this.value.length > 0){
+            for(let i = 0; i < pacientes.length; i++){
+        
+                let paciente = pacientes[i];
+                let tdNome = paciente.querySelector(".info-nome");
+                let nome = tdNome.textContent;
+        
+                if(nome != this.value){
+                    paciente.classList.add("invisivel");
+                }
+            }
+        }else{
+            for(let i = 0; i < pacientes.length; i++){
+                let paciente = pacientes[i];
+
+                paciente.classList.remove("invisivel");
+            }
+        }  
 });
